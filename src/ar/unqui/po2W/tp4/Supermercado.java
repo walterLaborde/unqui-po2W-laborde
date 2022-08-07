b67df58884d75f1b711ae3ce59c4a20e3f4d65ee
Walter <walter.laborde@hotmail.com>
2022-08-06 23:31:34 -0300

    Configuracion de la clase v1

---
+++ src/ar/unqui/po2W/tp4/Supermercado.java
@@ -0,0 +1,65 @@
+package ar.unqui.po2W.tp4;
+
+import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Supermercado {
+
+	private String nombre;
+	private String direccion;
+	private List<Producto> productos = new ArrayList<Producto>();
+	
+	public Supermercado() {
+		super();
+	}
+	
+	// Creation Parameter Method
+	public Supermercado setSupermercado(String nombre, String direccion) {
+		this.setNombre(nombre);
+		this.setDireccion(direccion);
+		return this;
+	}
+
+	//private Getters and Setters
+	private String getNombre() {
+		return nombre;
+	}
+
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	private String getDireccion() {
+		return direccion;
+	}
+
+	private void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+		
+	private List<Producto> getProductos() {
+		return productos;
+	}
+
+	private void setProductos(List<Producto> productos) {
+		this.productos = productos;
+	}
+	
+	//public methods
+
+	public int cantidadDeProductos() {
+		return this.getProductos().size();
+	}
+	
+	//this sums all product prices.
+	public Double sumatoriaPrecioTotalProductos() {
+		return this.getProductos().stream().collect(Collectors.summingDouble(p -> p.precio()));
+	}
+	
+	//this adds a product to the array list without giving access to the setter directly
+	public void agregarProducto(Producto p) {
+		this.getProductos().add(p);
+	}
+}
