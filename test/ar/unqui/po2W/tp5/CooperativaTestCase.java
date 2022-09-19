90bccafe215d111feb894e3f89ed5a0fb93def1e
Walter <walter.laborde@hotmail.com>
2022-09-19 16:37:47 -0300

    creacion test cooperativa

---
+++ test/ar/unqui/po2W/tp5/CooperativaTestCase.java
@@ -0,0 +1,14 @@
+package ar.unqui.po2W.tp5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CooperativaTestCase {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
