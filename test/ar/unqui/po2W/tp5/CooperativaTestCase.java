eb259d04383f04bac6266fb08b70f93b6ffb3be9
Walter <walter.laborde@hotmail.com>
2022-09-19 16:47:42 -0300

    test precio

--- test/ar/unqui/po2W/tp5/CooperativaTestCase.java
+++ test/ar/unqui/po2W/tp5/CooperativaTestCase.java
@@ -2,13 +2,22 @@ package ar.unqui.po2W.tp5;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CooperativaTestCase {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	private Cooperativa prodCoop1;
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		prodCoop1 = new Cooperativa(100,4,20);
 	}
+	
+	@Test
+	void testPrecio() {
+		assertEquals(prodCoop1.precio(),80);
+	}
+	
 
 }
