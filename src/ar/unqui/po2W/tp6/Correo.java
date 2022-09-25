c01a5a24dfe378852b9e3ed3fc9ecc69904b2eb5
Walter <walter.laborde@hotmail.com>
2022-09-25 20:31:13 -0300

    imports clases caso1

---
+++ src/ar/unqui/po2W/tp6/Correo.java
@@ -0,0 +1,9 @@
+package openClosedPrinciple.caso1;
+
+public class Correo {
+
+	public Correo(String asunto, String destinatario, String cuerpo) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
